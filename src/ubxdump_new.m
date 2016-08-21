61cfaa6cdd60651871638d26aaf0e00e923ae591
serg <srg.trustno1@gmail.com>
2016-08-22 02:21:09 +0300

    Draft implementation of RXM-RAWX message parser.

--- src/ubxdump_new.m
+++ src/ubxdump_new.m
@@ -1,6 +1,7 @@
-function res = ubxdump_new(filename)
-    res = zeros();
+function result = ubxdump_new(filename)
+    result = cell(0, 1);
     fileID = fopen(filename, 'r');
+    i = 1;
     
     if fileID == -1
         return;
@@ -8,7 +9,8 @@ function res = ubxdump_new(filename)
     
     while ~feof(fileID)
         if isSync(fileID) == 1
-            readPacket(fileID);
+            result{i} = readPacket(fileID);
+            i = i + 1;
         else
             % log
         end
@@ -35,7 +37,8 @@ function flag = isSync(fileID)
     flag = 1;
 end
 
-function readPacket(fileID)
+function result = readPacket(fileID)
+    result = {};
     packetStart = ftell(fileID);
     
     class = fread(fileID, 1, 'uint8');
@@ -69,43 +72,72 @@ function readPacket(fileID)
 
     if ckA == ckACalc && ckB == ckBCalc
         fseek(fileID, payloadStart, 'bof');
-        parsePayload(fileID, class, id, payloadLength);
+        result = parsePayload(fileID, class, id, payloadLength);
         fseek(fileID, 2, 'cof');
     else
         fprintf('Bad packet. Start: %d, L: %d\n', packetStart, payloadLength);
     end
 end
 
-function parsePayload(fileID, class, id, length)
+function result = parsePayload(fileID, class, id, length)
     switch class
         case 1 % NAV
-            parseNavClass(fileID, id, length);
+            result = parseNavClass(fileID, id, length);
         case 2 % RXM
-            parseRxmClass(fileID, id, length);
+            result = parseRxmClass(fileID, id, length);
         otherwise
             fprintf('Parser for class: %X not implemented\n', class);
     end
 end
 
-function parseNavClass(fileID, id, length)
+function result = parseNavClass(fileID, id, length)
     fprintf('NAV Class. Message ID: %X L:%d\n', id, length);
+    result = {};
 end
 
-function parseRxmClass(fileID, id, length)
+function result = parseRxmClass(fileID, id, length)
     switch id
         case 19 % RXM-SFRBX
-            parseRxmSfrbxMessage(fileID, length);
+            result = parseRxmSfrbxMessage(fileID, length);
         case 21 % RXM-RAWX
-            parseRxmRawxMessage(fileID, length);
+            result = parseRxmRawxMessage(fileID, length);
         otherwise
             fprintf('Parser for message ID: %X not implemented\n', id);
     end
 end
 
-function parseRxmSfrbxMessage(fileID, length)
+function result = parseRxmSfrbxMessage(fileID, length)
     fprintf('RXM-SFRBX Message. L:%d\n', length);
+    result = {};
 end
 
-function parseRxmRawxMessage(fileID, length)
+function result = parseRxmRawxMessage(fileID, length)
     fprintf('RXM-RAWX Message. L:%d\n', length);
+    result = {};
+    result.rcvTow = fread(fileID, 1, 'double');
+    result.week = fread(fileID, 1, 'uint16');
+    result.leapS = fread(fileID, 1, 'int8');
+    numMeas = fread(fileID, 1, 'uint8');
+    result.recStat = fread(fileID, 1, 'uint8');
+    result.reserved1 = fread(fileID, 3, 'uint8');
+    measurments = cell(numMeas, 1);
+    for i = 1:numMeas
+        meas = {};
+        meas.prMes = fread(fileID, 1, 'double');
+        meas.cpMes = fread(fileID, 1, 'double');
+        meas.doMes = fread(fileID, 1, 'single');
+        meas.gnssId = fread(fileID, 1, 'uint8');
+        meas.svId = fread(fileID, 1, 'uint8');
+        meas.reserved2 = fread(fileID, 1, 'uint8');
+        meas.freqId = fread(fileID, 1, 'uint8');
+        meas.locktime = fread(fileID, 1, 'uint16');
+        meas.cno = fread(fileID, 1, 'uint8');
+        meas.prStdev = fread(fileID, 1, 'uint8');
+        meas.cpStdev = fread(fileID, 1, 'uint8');
+        meas.doStdev = fread(fileID, 1, 'uint8');
+        meas.trkStat = fread(fileID, 1, 'uint8');
+        meas.reserved3 = fread(fileID, 1, 'uint8');
+        measurments{i} = meas;
+    end
+    result.measurments = measurments;
 end
