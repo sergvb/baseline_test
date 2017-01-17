de6e163ccac5bd9e01e16a5ba3ff6d6ad07302f8
serg <srg.trustno1@gmail.com>
2017-01-17 23:37:10 +0300

    Just to save the current changes.

--- src/main.m
+++ src/main.m
@@ -1,11 +1,27 @@
 clc, clear all;
 
-filter = [ubx.Message.RXM_RAWX, ubx.Message.NAV_POSECEF];
+fileId = fopen('..\data\RS_matv_50mm_01.bin');
 
-dataA = readData('..\data\RS_matv_50mm_01.bin', filter);
-dataB = readData('..\data\RS_matv_50mm_02.bin', filter);
+rawxPacket = readNextPacket(fileId, ubx.Message.RXM_RAWX);
+gpsMeasIndex = find([rawxPacket.measurments.gnssId] == 0);
+gpsSv = [rawxPacket.measurments(gpsMeasIndex).svId];
 
-groupA = groupPackets(dataA);
-groupB = groupPackets(dataB);
+sfData = zeros(11, numel(gpsSv), 'uint32');
+sfData(1, :) = gpsSv;
 
-X = computeDifferences(groupA, groupB);
+notBindedSv = gpsSv;
+while ~isempty(notBindedSv) && ~feof(fileId)
+    sfrbxPacket = readNextPacket(fileId, ubx.Message.RXM_SFRBX);
+    
+    if ~isempty(fieldnames(sfrbxPacket)) && sfrbxPacket.gnssId == 0
+        notBindedsSvIndex = find(notBindedSv == sfrbxPacket.svId);
+        svIndex = find(gpsSv == sfrbxPacket.svId);
+
+        if ~isempty(notBindedsSvIndex)
+            sfData(2 : end, svIndex) = sfrbxPacket.words;
+            notBindedSv(notBindedsSvIndex) = [];
+        end
+    end
+end
+
+fclose all;
