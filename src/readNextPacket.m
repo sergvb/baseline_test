b11d638752e2ca26d2648157707dfc1d973d6672
serg <srg.trustno1@gmail.com>
2017-01-21 15:23:23 +0300

    Cleanup.

--- src/readNextPacket.m
+++
@@ -1,9 +0,0 @@
-function packet = readNextPacket(fileID, packetID)
-    while ~feof(fileID)
-        packet = ubx.readPacket(fileID);
-
-        if ~isempty(fieldnames(packet)) && packetID == packet.Id
-            return;
-        end
-    end
-end
